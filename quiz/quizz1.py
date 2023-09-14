92ebf131724bb11e507586f40a65c14e6707c8c1
XxNoLimitTayxX <145042035+XxNoLimitTayxX@users.noreply.github.com>
2023-09-14 17:01:12 +0000

    done

--- quiz/quizz1.py
+++ quiz/quizz1.py
@@ -1,14 +1,18 @@
 # 1. In your own words, describe what a variable is.
-#A variable is a storage holder (container) for information.
+"A variable is a storage holder (container) for information."
 
 # 2. Name three (3) operators we've learned about in class and describe 
+"Logical Operator, Comparison Operator, Assignment Operator"
 # a situation where you would use each one.
- 
+ "Comparison Operator: Dog > Cat (weight)"
+ "Logical Operator: True or False? Your age is over 10"
+ "Mathmatic Operator: 1+1"
 
 # 3. Describe the difference between camal case and pascal case.
+ "The only diference is that the first letter of the first word is capitalized in the Pascal case unlike Camel"
 
 # 4. Which operator is being used in the example below
-#Intergers 
+ "Intergers"
 
 numberOfApples = 10
 numberOfOranges = 12
@@ -20,8 +24,16 @@ numberOfApples < numberOfOranges
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
 
+moneyFromFam1 = 100
+moneyFromFam2 = 50
+moneyFromFam3 = 35
+
+totalAmounrOfMoney = 185
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+
+userName = imput("What is your name?")
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
@@ -29,14 +41,21 @@ numberOfApples < numberOfOranges
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
 
+fullName = imput("What's your full name?")
+yourAge = imput("How old are you?")
+allergies = imput("Do you have any allergies? If so, what?")
+favoriteFood = imput("What's your favorite food?")
+
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
 
+glutenAllergy =imput("Do you have a gluten allergy?")
+"You could use a boolean to determine if it's true or false."
+
 # 9. In your own words, describe what a program is?
-
+"A function is a command that allows something to work through a program."
 # 10. Describe the difference between a float and an integer. 
-
-#testing update
+"An interger is a whole number while a float is a number with decimals."
