373fe59c3ae0be91ba765db6e869d23222e4447b
XxNoLimitTayxX <145042035+XxNoLimitTayxX@users.noreply.github.com>
2023-09-29 16:52:46 +0000

    Act2

--- IntroPythonActivities/ConditionalActivity2.py
+++ IntroPythonActivities/ConditionalActivity2.py
@@ -9,6 +9,17 @@
 # your function should also alow the user to put in the time. The time should be 
 # formatted as a float. 
 
+def periodEnding():
+    periodEnding
+
+currentTime = 12.00
+periodEnding = 11.5
+
+if currentTime > periodEnding:
+    print("Class is over. Time to go!")
+else:
+    print("Class is still in session")
+
 # 2. Write a function that uses a conditional statement. 
 # your function should determine what type a pet a user has depeding on the data provided by the user
 # passed into the functions argument. 
@@ -17,8 +28,20 @@
 # ELSE, if it is none of the animal sounds the function should return a message saying it doesn't 
 # know what the animal is. 
 
+
+userSound = "woof (bark)"
+
+def typeOfPet():
+    userSound = ("What Pet is This?")
+    if userSound == "woof (bark)":
+        print("Dog")
+    else:
+        print("cat")
+
+
 # 3. Write a function that will take in a user name and height as parameters. 
 # Your function should evaluate and determine if the user is tall in enough to get on a roller coster.
 # IF the user is over 5.5, the function should return a custom message saying the user's name
 # and a message "welcome please buckle up".
 # ELSE if they they are not, return a message apologizing to the user saying they are not tall enough.
+
