a2f030fcc59c838402132000682ef107aa0fb106
XxNoLimitTayxX <145042035+XxNoLimitTayxX@users.noreply.github.com>
2023-10-18 16:59:50 +0000

    loop prac

---
+++ IntroPythonActivities/loopPractice.py
@@ -0,0 +1,8 @@
+def continueExample():
+    i=0
+    while i <= 8: 
+        i+=1
+        if i==5:
+            print('skip the number 5. ')
+
+
--- IntroPythonActivities/readingActivities.py
+++
@@ -1,3 +0,0 @@
-# read the following articles on Python functions
-"https://www.freecodecamp.org/news/functions-in-python-a-beginners-guide"
-
