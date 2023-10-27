77e7cb42af638d0a4eed4ee868a13195e02f21a8
XxNoLimitTayxX <145042035+XxNoLimitTayxX@users.noreply.github.com>
2023-10-27 16:16:52 +0000

    3 Questions Commit

---
+++ IntroPythonActivities/IntroPythonActivities/cesiumQuestions.py
@@ -0,0 +1,12 @@
+# Research and develop three (3) questions for Cesium.
+# Your questions should be relevant and appropriate for a workplace setting.
+# Your questions will be screened prior to leaving for the trip. 
+# Questions must be submitted before the end of class for credit. 
+
+# https://cesium.com/
+
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+# 1. What was the hardest project you have went through and what was so hard about it?
+# 2. How do you get started on a new project? What are the typical steps?
+# 3. How long did it take for you to get used to the advanced levels of coding?
