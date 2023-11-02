61dc64a1c738f1d9f5646c38a30c5df1f3db3a2e
XxNoLimitTayxX <145042035+XxNoLimitTayxX@users.noreply.github.com>
2023-11-02 16:42:35 +0000

    gitCloneAct 11-2-23

---
+++ GitCloneAct/gitCloneAct.py
@@ -0,0 +1,8 @@
+def thingy():
+
+    num1 = int(input('what is the first number you would like to add??'))
+    num2 = int(input('what is the second number you would like to add??'))
+    print(num1 + num2)
+
+thingy()
+
